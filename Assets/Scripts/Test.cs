6994977fa51b825ea4349a180ded523c7cd3405e
JLeoAI <jamesleohoyle@googlemail.com>
2020-09-08 16:15:57 +0100

    test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int test;
+    private int test2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = 4;
+        test2 = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
